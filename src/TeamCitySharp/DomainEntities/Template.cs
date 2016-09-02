6cb24d0c959066a31e799a415f983b674beed08b
Ryan Gribble <ryan.gribble@tattsgroup.com>
2016-09-02 14:55:33 +1000

    Add more fields to BuildConfig Template

--- src/TeamCitySharp/DomainEntities/Template.cs
+++ src/TeamCitySharp/DomainEntities/Template.cs
@@ -1,20 +1,20 @@
 ﻿namespace TeamCitySharp.DomainEntities
 {
-  public class Template
-  {
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public string Href { get; set; }
-    public string ProjectId { get; set; }
-    public string ProjectName { get; set; }
-
-    public Template()
+    public class Template
     {
-      Id = "";
-      Name = "";
-      Href = "";
-      ProjectId = "";
-      ProjectName = "";
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Href { get; set; }
+        public string ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public Project Project { get; set; }
+        public Parameters Parameters { get; set; }
+        public ArtifactDependencies ArtifactDependencies { get; set; }
+        public SnapshotDependencies SnapshotDependencies { get; set; }
+        public VcsRootEntries VcsRootEntries { get; set; }
+        public BuildSteps Steps { get; set; }
+        public AgentRequirements AgentRequirements { get; set; }
+        public BuildTriggers Triggers { get; set; }
+        public Properties Settings { get; set; }
     }
-  }
 }
